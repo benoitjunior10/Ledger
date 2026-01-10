3e3055ea512f5beeba1f23b62ccbf853378dbc3d
benoitjunior10 <benoitjeanrobertjunior@gmail.com>
2026-01-09 22:04:14 -0500

    Enleve des infos qui sont sense etre dans le fichier .env

--- db.js
+++ db.js
@@ -4,8 +4,8 @@ require('dotenv').config();
 const pool = mysql.createPool({
     host: process.env.DB_HOST || 'localhost',
     user: process.env.DB_USER || 'root',
-    password: process.env.DB_PASSWORD || 'Sierra@0000#',
-    database: process.env.DB_NAME || 'haitipay_db',
+    password: process.env.DB_PASSWORD ,
+    database: process.env.DB_NAME ,
     waitForConnections: true,
     connectionLimit: 10,
     queueLimit: 0
